4cd61fcae62936ebf441ede1495bd0f435140a74
조용제 <whehdrms6810@naver.com>
2022-12-02 16:36:25 +0900

    [ADD] widgets

--- iot/lib/screens/bottombar/trash.dart
+++ iot/lib/screens/bottombar/trash.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Trash extends StatefulWidget {
   const Trash({super.key});
@@ -9,6 +10,8 @@ class Trash extends StatefulWidget {
 }
 
 class _TrashState extends State<Trash> {
+  final List<Widget> carouselList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +37,44 @@ class _TrashState extends State<Trash> {
                   )
                 ],
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 19.w, bottom: 39.h, top: 48.h),
+                  child: Text(
+                    "분리수거 선택",
+                    style:
+                        TextStyle(fontSize: 30.sp, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            ),
+
+            Container(
+              width: 260.w,
+              height: 400.h,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 1.w),
+                  borderRadius: BorderRadius.all(Radius.circular(10.r))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    ],
+                  ),
             )
+            //     Container(
+
+            // child: CarouselSlider(
+            //   options: CarouselOptions(
+            //     autoPlay: true,
+            //     aspectRatio: 2.0,
+            //     enlargeCenterPage: true,
+            //   ),
+            //   items: carouselList,
+            // ),)
           ],
         ),
       ),
