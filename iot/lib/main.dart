5d27e682be96ccfd292393e5a11f68cf7bf455f5
조용제 <whehdrms6810@naver.com>
2022-12-03 13:39:43 +0900

    [ADD] multiprovider, screenutil

--- iot/lib/main.dart
+++ iot/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iot/provider/user.dart';
 import 'package:iot/screens/bottombar/bottom.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyAPp());
@@ -14,14 +16,21 @@ class MyAPp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: [SystemUiOverlay.bottom]);
-    return ScreenUtilInit(
-      //screenutil 라이브러리 (뒤에 .h, .w, .r, .sp등등 크기를 반응형으로 만들어줌)
-      designSize: const Size(390, 844), // 어떤 사이즈를 기준으로 만들것인가
-      builder: (BuildContext context, Widget? child) => const MaterialApp(
-          title: 'Welcome',
-          debugShowCheckedModeBanner: false,
-          home: Login() // widget/bottombar.dart 코드
-          ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Emphaisis>(
+          create: (_) => Emphaisis(),
+        ),
+      ],
+      child: ScreenUtilInit(
+        //screenutil 라이브러리 (뒤에 .h, .w, .r, .sp등등 크기를 반응형으로 만들어줌)
+        designSize: const Size(390, 844), // 어떤 사이즈를 기준으로 만들것인가
+        builder: (BuildContext context, Widget? child) => const MaterialApp(
+            title: 'Welcome',
+            debugShowCheckedModeBanner: false,
+            home: Bottom() // widget/bottombar.dart 코드
+            ),
+      ),
     );
   }
 }
